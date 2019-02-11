3634ce57a5ab79ba20c6ffc68f5616da794355f6
shaifulullah <shaiful.ullah@yahoo.com>
2019-02-10 20:55:10 -0500

    Added toggle content using State

--- tryreacttutorial/src/posts/PostDetail.js
+++ tryreacttutorial/src/posts/PostDetail.js
@@ -5,6 +5,10 @@ export default class PostDetail extends Component {
     constructor(props) {
         super(props)
         this.titleWasClicked = this.titleWasClicked.bind(this)
+        this.toggleContent = this.toggleContent.bind(this)
+        this.state = {
+            showContent: false,
+        }
     }
     titleWasClicked(event) {
         event.preventDefault()
@@ -12,13 +16,24 @@ export default class PostDetail extends Component {
         const { dataCallback } = this.props; //calling my dataCallback while clicking on title
         dataCallback("hello world testing")
     }
+    toggleContent(event) {
+        event.preventDefault()
+        this.setState({
+            showContent: !this.state.showContent
+        })
+    }
     render() {
         const { post } = this.props
+        const { showContent } = this.state
         return (
             <div>
                 <h1 onClick={this.titleWasClicked}>{post.title} </h1>
-                <p>{post.content} </p>
+                {showContent === true ? <p>{post.content} </p> : ' '}
+                {/* //in line number31 if we want to show class then use below
+                    // {<p className= {`${showContent === true ? 'd-block ': 'd-none'}`} > {post.content} </p>} */}
+                <button onClick={this.toggleContent}>Show content</button>
             </div>
         )
     }
 }
+
